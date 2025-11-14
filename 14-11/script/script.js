ca88510ae6e30b535ee949ace8ab21f819769e27
Giovanni Raniolo <gianniraniolo80@gmail.com>
2025-11-14 17:27:15 +0100

    feat: add prompt-alert.html and script.js

---
+++ 14-11/script/script.js
@@ -0,0 +1,8 @@
+// let name = prompt("Benvenuto, qual è il tuo nome?");
+// console.log("Il nome inserito è: " + name);
+// alert("Ciao " + name + ", benvenuto nel mondo di JavaScript!");
+
+function salutaUtente() {
+  let nomeUtente = document.getElementById("nomeUtente").value;
+  alert("Ciao " + nomeUtente + ", benvenuto nel nostro sito!");
+}
